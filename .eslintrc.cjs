22df564694bbff228a86268f7f3a28e777f92707
Jamal NR <jamalnr@MacBook-Air-Jamal.local>
2025-08-20 08:49:42 +0700

    Restore monorepo hasil copy yang sukses

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,20 @@
 module.exports = {
   root: true,
-  env: { node: true, es2022: true },
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  ignorePatterns: ['node_modules/', 'dist/', 'build/', '.next/', 'out/'],
-  rules: { '@typescript-eslint/no-explicit-any': 'off' }
-};
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  settings: {
+    react: { version: 'detect' }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+  ignorePatterns: ['dist', 'node_modules', '.next'],
+}
